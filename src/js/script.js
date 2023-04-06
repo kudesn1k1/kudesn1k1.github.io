d4de69b6aefe2a12a2f079c2a07b78ba4ffd586d
kudesn1k1 <68770239+kudesn1k1@users.noreply.github.com>
2023-04-06 13:16:45 +0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,68 @@
 $(document).ready(function () {
+	$(window).scroll(function () {
+		let distance = $(window).scrollTop();
 
-    $(window).scroll(function () {
-        let distance = $(window).scrollTop();
+		$(".section").each(function (i, el) {
+			if (($(el).offset().top - $("nav").outerHeight()) <= distance) {
 
-        $(".section").each(function (i, el) {
-            if (($(el).offset().top - $("nav").outerHeight()) <= distance) {
+				$("nav a").each(function (i, el) {
+					if ($(el).hasClass("act")) {
+						$(el).removeClass("act");
+					}
+				});
 
-                $("nav a").each(function (i, el) {
-                    if ($(el).hasClass("act")) {
-                        $(el).removeClass("act");
-                    }
-                });
+				$("nav li:eq(" + i + ")").find("a").addClass("act");
+			}
+		});
+	}); //Отображение текущей секции
 
-                $("nav li:eq(" + i + ")").find("a").addClass("act");
-            }
-        });
-    }); //Отображение текущей секции
-
-    $("nav a").click(function () {
-        let valRef = $(this).attr("href");
-        $("html,body").animate({
-            scrollTop: $(valRef).offset().top - 40 + "px"
-        });
-    }); //якорные ссылки
-
+	$("nav a, .title a").click(function () {
+		let valRef = $(this).attr("href");
+		$("html,body").animate({
+			scrollTop: $(valRef).offset().top - 40 + "px"
+		});
+	}); //якорные ссылки
 
 
-    $('.image-link').magnificPopup({
-        type: 'image'
-    }); //modal images
-    setTimeout(function(){$("#exampleModal").modal("show");}, 10000)
-    
 
-});
+	$('.image-link').magnificPopup({
+		type: 'image'
+	}); //modal images
+	setTimeout(function () { $("#exampleModal").modal("show"); }, 10000)
+
+	// $("#phone").mask("+7(999) 999-99-99");
+
+	$(".dropdown li a").click(function () {
+		$(".ham").removeClass("active");
+	})
+
+	$(window).click(function (event) {
+		if (!(event.target.matches(".ham") || event.target.matches(".line"))) {
+			$(".ham").removeClass("active");
+		}//закрываем выпадающий список при клике вне выпадающего окна
+
+	});
+
+	// 	$("#phone").change(function () {
+	// 		const regex = /^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/;
+	// 		let val = $(this).val();
+	// 		if (!regex.test(val)) {
+	// 			$(this).css("background-color", "rgba(255,0,0,0.7)");
+	// 			$(".button").prop('disabled', 'disabled');
+	// 		}
+	// 	});
+
+	$("form").submit(function (event) {
+		event.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "php/mail.php",
+			data: $("this").serialize()
+		}).done(function () {
+			$("this").find("input").val('');
+			alert("Успешно отправлено!");
+			$("form").trigger("reset");
+		});
+		return false;
+	})
+})
--- src/js/scroll_script.js
+++ src/js/scroll_script.js
@@ -1,20 +1,7 @@
 function onEntry(entry) {
     entry.forEach(change => {
         if (change.isIntersecting) {
-            change.target.classList.add('element-show');
-            /*if (change.target.classList.contains("num")) {
-                let finish = parseFloat(change.target.innerHTML);
-                let i = 0;
-                function run(){
-                    change.target.innerHTML = "<h3 class='mb-3 num'>" + i + "</h3>";
-                    if(change.target.classList.contains("num1")){i+=40;}
-                    else if(change.target.classList.contains("num2")){i+=5;}
-                    else{i++;}
-                    if(i<=finish){setTimeout(run, 15);}
-                }
-                run();
-            }*/
-            
+            change.target.classList.add('element-show');         
         }
     });
 };
@@ -34,7 +21,6 @@ for (let elm of elements) {
 document.addEventListener("DOMContentLoaded", function () {
     var lazyloadImages;
 
-    //if ("IntersectionObserver" in window) {
     lazyloadImages = document.querySelectorAll(".lazy");
     var imageObserver = new IntersectionObserver(function (entries) {
         entries.forEach(function (entry) {
@@ -51,3 +37,4 @@ document.addEventListener("DOMContentLoaded", function () {
         imageObserver.observe(image);
     });//отложенная загрузка изображений
 })
+
